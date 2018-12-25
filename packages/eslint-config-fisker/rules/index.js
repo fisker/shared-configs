1ccfd15e0236238bf1c85640c092700466416aeb
fisker <lionkay@gmail.com>
2018-12-25 15:44:17 +0800

    for...in

--- packages/eslint-config-fisker/rules/index.js
+++ packages/eslint-config-fisker/rules/index.js
@@ -77,6 +77,18 @@ module.exports = {
       {
         allowForLoopAfterthoughts: true
       }
+    ],
+    // allow for...in and for...of
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'LabeledStatement',
+        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
     ]
   }
 }
