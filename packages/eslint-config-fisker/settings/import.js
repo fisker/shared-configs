5c8a6ea081a840f7e042b1c0025ebf2c57e2cba6
fisker <lionkay@gmail.com>
2019-01-14 17:45:49 +0800

    remove resolver

--- packages/eslint-config-fisker/settings/import.js
+++ packages/eslint-config-fisker/settings/import.js
@@ -1,10 +1,5 @@
 module.exports = {
   settings: {
-    'import/resolver': {
-      webpack: {
-        config: require.resolve('@vue/cli-service/webpack.config.js')
-      }
-    },
     'import/extensions': ['.js', '.jsx', '.mjs', '.ts', '.tsx']
   }
 }
