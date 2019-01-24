87ed938152f2241b3f773a63d5bdeb7a43962be6
fisker <lionkay@gmail.com>
2019-01-24 14:51:19 +0800

    add eslint

--- packages/eslint-config-fisker/rules/vue.js
+++ packages/eslint-config-fisker/rules/vue.js
@@ -1,10 +1,17 @@
 module.exports = {
   plugins: ['vue'],
-  extends: ['plugin:vue/recommended'],
+  extends: ['plugin:vue/strongly-recommended'],
   rules: {
     // allow unused vars
     'vue/no-unused-vars': 'warn',
     // allow unused components
-    'vue/no-unused-components': 'warn',
+    'vue/no-unused-components': [
+      'warn',
+      {
+        ignoreWhenBindingPresent: true,
+      },
+    ],
+    // https://vuejs.github.io/eslint-plugin-vue/rules/this-in-template.html
+    'vue/this-in-template': ['error', 'never'],
   },
 }
