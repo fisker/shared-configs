65dd054383e398b6a1a7f2f4116a79fa872cbb59
fisker <lionkay@gmail.com>
2019-01-30 18:48:58 +0800

    enable curly

--- packages/eslint-config-fisker/configs/prettier/prettier.js
+++ packages/eslint-config-fisker/configs/prettier/prettier.js
@@ -2,6 +2,9 @@ module.exports = {
   plugins: ['prettier'],
   extends: ['eslint-config-prettier'].map(require.resolve),
   rules: {
+    // specify curly brace conventions for all control statements
+    curly: ['error', 'all'],
+
     'prettier/prettier': 'warn',
   },
 }
