35644ed0eec6eac0f923165140acb1d54ff868a9
fisker <lionkay@gmail.com>
2019-02-22 11:05:56 +0800

    chore(config): update configs

--- packages/eslint-config-fisker/prettier.config.js
+++ packages/eslint-config-fisker/prettier.config.js
@@ -1,5 +1,11 @@
-// options https://prettier.io/docs/en/options.html
-// Configuration File https://prettier.io/docs/en/configuration.html
+/*!
+ * config file for prettier
+ * project https://github.com/xwtec/dotfiles
+ * primary link https://raw.githubusercontent.com/xwtec/dotfiles/master/prettier/prettier.config.js
+ *
+ * options https://prettier.io/docs/en/options.html
+ *
+ */
 
 module.exports = {
   // options for all files
@@ -12,9 +18,54 @@ module.exports = {
   // overrides
   overrides: [
     {
-      files: '*.js',
+      files: '*.{js,mjs}',
       options: {
         parser: 'babel',
+      },
+    },
+    {
+      files: '.ts',
+      options: {
+        parser: 'typescript',
+      },
+    },
+    {
+      files: '*.{css,less,scss}',
+      options: {
+        parser: 'css',
+        singleQuote: false,
+      },
+    },
+    {
+      files: '*.scss',
+      options: {
+        parser: 'scss',
+        singleQuote: false,
+      },
+    },
+    {
+      files: '*.less',
+      options: {
+        parser: 'less',
+        singleQuote: false,
+      },
+    },
+    {
+      files: '*.css',
+      options: {
+        parser: 'css',
+      },
+    },
+    {
+      files: '*.vue',
+      options: {
+        parser: 'vue',
+      },
+    },
+    {
+      files: '*.{html,htm}',
+      options: {
+        parser: 'html',
       },
     },
     {
@@ -24,9 +75,29 @@ module.exports = {
       },
     },
     {
+      files: '*.json5',
+      options: {
+        parser: 'json5',
+      },
+    },
+    {
       files: '*.{md,markdown}',
       options: {
         parser: 'markdown',
+        singleQuote: false,
+      },
+    },
+    {
+      files: '*.{yaml,yml}',
+      options: {
+        parser: 'yaml',
+        singleQuote: false,
+      },
+    },
+    {
+      files: '*.{graphql,gql}',
+      options: {
+        parser: 'graphql',
       },
     },
   ],
