7aa24c76fe382287e141870373268c3ce3e57d72
fisker <lionkay@gmail.com>
2019-02-22 11:05:11 +0800

    style(vue): add dev notice

--- packages/eslint-config-fisker/vue.js
+++ packages/eslint-config-fisker/vue.js
@@ -1,6 +1,9 @@
-// don't use @vue/eslint-config-prettier
-// eslint:recommended breaks airbnb
-// https://github.com/vuejs/vue-cli/blob/dev/packages/@vue/eslint-config-prettier/index.js
+/**
+ * dev notice:
+ * don't use @vue/eslint-config-prettier
+ * eslint:recommended extend in this config breaks airbnb
+ * check this https://github.com/vuejs/vue-cli/blob/dev/packages/@vue/eslint-config-prettier/index.js
+ */
 
 module.exports = {
   extends: [
