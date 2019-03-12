71b71d4d4b3d6d78272570696ac9d6aa3523e7e4
fisker <lionkay@gmail.com>
2019-03-12 10:59:22 +0800

    fix(test): fix test

--- packages/renovate-config-fisker/__tests__/test.js
+++ packages/renovate-config-fisker/__tests__/test.js
@@ -1,20 +1,11 @@
 import test from 'ava'
-import m from '..'
+import config from '..'
 import pkg from '../package.json'
-
-test('automerge is set', t => {
-  t.true(m.default.devDependencies.automerge)
-})
-
-test('lockFileMaintenance is set', t => {
-  t.true(m.default.lockFileMaintenance.enabled)
-  t.true(m.default.lockFileMaintenance.automerge)
-})
 
 test('package.json', t => {
   t.deepEqual(
     pkg['renovate-config'],
-    m,
+    config,
     '`renovate-config` in `package.json` is invalid'
   )
 })
