3f508675c7470e5b3c5e5716ac0bcd466fa496a8
fisker <lionkay@gmail.com>
2019-04-17 14:12:37 +0800

    feat: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+/*!
+ * config file for `eslint`
+ *
+ * update: wget -O .eslintrc.js https://git.io/fjJKA
+ * document: https://eslint.org/docs/user-guide/configuring
+ */
+
+/* eslint-config-fisker https://git.io/fjJKy */
+
+module.exports = {
+  root: true,
+  parserOptions: {},
+  extends: ['fisker'],
+  settings: {},
+  rules: {},
+  plugins: [],
+  overrides: [],
+}
