2be01550d312af4f02f9195bca7853f53a942e59
fisker <lionkay@gmail.com>
2019-04-22 09:49:47 +0800

    fix: disable `system-ui-font-family`

--- packages/postcss-config/src/index.js
+++ packages/postcss-config/src/index.js
@@ -1,6 +1,11 @@
 import postcssPresetEnvironment from 'postcss-preset-env'
 
-const postcssPresetEnvironmentPlugin = postcssPresetEnvironment({stage: 0})
+const postcssPresetEnvironmentPlugin = postcssPresetEnvironment({
+  stage: 0,
+  features: {
+    'system-ui-font-family': false,
+  },
+})
 
 export default {
   plugins: [postcssPresetEnvironmentPlugin],
