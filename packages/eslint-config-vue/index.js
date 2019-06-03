f1f4a5ed5d322733e6eaca02dd3f15ab0fc9b796
fisker <lionkay@gmail.com>
2019-06-03 16:42:50 +0800

    fix: fix eslin-config-vue parserOptions

--- packages/eslint-config-vue/index.js
+++ packages/eslint-config-vue/index.js
@@ -1,5 +1,15 @@
 module.exports = {
   root: true,
+  // stupid eslint-plugin-vue override parserOptions
+  // https://github.com/vuejs/eslint-plugin-vue/blob/c3111fed451807eb92dc3f5020d144fbd2af9fda/lib/configs/base.js#L8
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
+    ecmaFeatures: {
+      globalReturn: false,
+      jsx: true,
+    },
+  },
   extends: [
     '@fisker/eslint-config',
     './plugins/vue.js',
