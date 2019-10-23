78efc3715f003d0f231b61233aedf004edb5a282
fisker <lionkay@gmail.com>
2019-10-23 11:04:41 +0800

    feat(lint-staged): add `--fix` to markdownlint

--- packages/lint-staged-config/src/commands.js
+++ packages/lint-staged-config/src/commands.js
@@ -1,5 +1,5 @@
 export const PRETTIER = 'prettier --write'
 export const ESLINT = 'eslint --fix'
 export const STYLELINT = 'stylelint --fix'
-export const MARKDOWNLINT = 'markdownlint'
+export const MARKDOWNLINT = 'markdownlint --fix'
 export const GIT_ADD = 'git add'
