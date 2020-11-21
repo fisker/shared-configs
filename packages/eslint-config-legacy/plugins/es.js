7da6389705872b4ae5aab43e123fa880918ba1e9
fisker <lionkay@gmail.com>
2020-11-22 01:48:30 +0800

    chore: update deps

--- packages/eslint-config-legacy/plugins/es.js
+++ packages/eslint-config-legacy/plugins/es.js
@@ -1,10 +1,4 @@
 module.exports = {
   plugins: ['es'],
-  extends: [
-    'plugin:es/no-2018',
-    'plugin:es/no-2017',
-    'plugin:es/no-2016',
-    'plugin:es/no-2015',
-    'plugin:es/no-5',
-  ],
+  extends: ['plugin:es/restrict-to-es3'],
 }
