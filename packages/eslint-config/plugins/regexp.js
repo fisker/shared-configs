e3006daa96f72da61b200fc087a44616e9b8097c
fisker Cheung <lionkay@gmail.com>
2021-07-27 13:32:29 +0800

    Enable all `eslint-plugin-regexp` rules

--- packages/eslint-config/plugins/regexp.js
+++ packages/eslint-config/plugins/regexp.js
@@ -1,7 +1,5 @@
 module.exports = {
   plugins: ['regexp'],
-  extends: ['plugin:regexp/recommended'],
-  rules: {
-    'regexp/no-useless-flag': 'error',
-  },
+  extends: ['plugin:regexp/all'],
+  rules: {},
 }
