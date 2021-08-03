e80448808fcee4bc25fbc9cb0789515d7926cad8
fisker Cheung <lionkay@gmail.com>
2021-08-03 14:27:36 +0800

    Disable `regexp/sort-character-class-elements`

--- packages/eslint-config/plugins/regexp.js
+++ packages/eslint-config/plugins/regexp.js
@@ -4,5 +4,11 @@ module.exports = {
   rules: {
     // https://ota-meshi.github.io/eslint-plugin-regexp/rules/no-super-linear-move.html
     'regexp/no-super-linear-move': 'off',
+
+    // Alias of `regexp/sort-character-class-elements`
+    'regexp/order-in-character-class': 'off',
+
+    // Conflicting with `unicorn/better-regex`
+    'regexp/sort-character-class-elements': 'off',
   },
 }
