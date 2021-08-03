1b3bd59e019df5f89274b1f984f6f00a35ae24c8
fisker Cheung <lionkay@gmail.com>
2021-08-03 15:42:22 +0800

    More `import/*` rules

--- packages/eslint-config/plugins/import.js
+++ packages/eslint-config/plugins/import.js
@@ -1,11 +1,15 @@
 module.exports = {
   plugins: ['import'],
   extends: [
+    'plugin:import/recommended',
     'plugin:import/errors',
     'plugin:import/warnings',
+    'plugin:import/stage-0',
     require.resolve('../rules/imports.js'),
   ],
   rules: {
     'import/first': 'error',
+    'import/extensions': ['error', 'always', {ignorePackages: true}],
+    'import/order': 'error',
   },
 }
