7e299b4420cfe1e3d3a2e5bfeb6433ee76f9b37a
fisker Cheung <lionkay@gmail.com>
2021-11-22 09:34:40 +0800

    Use relative parser

--- packages/stylelint-config/vue.js
+++ packages/stylelint-config/vue.js
@@ -6,6 +6,6 @@ module.exports = {
     './prettier.js',
   ].map((module) => require.resolve(module)),
   // The customSyntax from `stylelint-config-recommended-vue` is override by `./scss.js`
-  customSyntax: 'postcss-html',
+  customSyntax: require.resolve('postcss-html'),
   rules: {},
 }
