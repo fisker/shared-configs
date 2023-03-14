0e8c5b4b8e2daa0f64ac3197dfc025f9c2a41663
fisker <lionkay@gmail.com>
2023-03-14 19:12:32 +0800

    Remove `stylelint-prettier`

--- packages/stylelint-config/prettier.js
+++ packages/stylelint-config/prettier.js
@@ -1,8 +1,5 @@
 module.exports = {
   plugins: ['stylelint-prettier'].map((module) => require.resolve(module)),
-  extends: ['stylelint-config-prettier'].map((module) =>
-    require.resolve(module),
-  ),
   rules: {
     'prettier/prettier': true,
   },
