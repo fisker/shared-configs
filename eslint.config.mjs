a127a86b34fb2926c6cc065653cc9e428e9d2c54
fisker <lionkay@gmail.com>
2024-06-05 15:22:56 +0800

    Ignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,35 @@
 import fiskerEslintConfig from './packages/eslint-config/index.js'
 
-export default fiskerEslintConfig
+export default [
+  ...fiskerEslintConfig,
+  {
+    ignores: `
+# also lint dot files
+!.*
+
+# vendors
+**/node_modules/**
+**/vendors/**
+**/vendor/**
+**/third-party/**
+
+# build file
+**/dist/**
+**/*.min.*
+
+# fixtures
+**/fixtures/**
+
+# test
+**/.nyc_output/**
+**/coverage/**
+
+# yarn
+**/.yarn
+
+# project glob
+`
+      .split('\n')
+      .filter((pattern) => pattern && !pattern.startsWith('#')),
+  },
+]
