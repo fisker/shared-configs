8a4d25c573fff773637c6b05058e5fe7a1ce7ef0
fisker <lionkay@gmail.com>
2026-01-02 21:46:06 +0800

    Add `@fisker/release-it-config`

---
+++ packages/release-it-config/index.cjs
@@ -0,0 +1,43 @@
+const fs = require('node:fs')
+const path = require('node:path')
+
+function* iterateDirectoryUp(directory) {
+  const stopDirectory = path.parse(directory).root
+
+  while (directory !== stopDirectory) {
+    yield directory
+    directory = path.dirname(directory)
+  }
+
+  yield stopDirectory
+}
+
+const hasPublishWorkflow = () => {
+  for (const directory of iterateDirectoryUp(__dirname)) {
+    if (fs.existSync(path.join(directory, '.git'))) {
+      return fs.existSync(path.join(directory, '.github/workflows/publish.yml'))
+    }
+  }
+
+  return false
+}
+
+let cachedHasPublishWorkflow
+
+const config = {
+  git: {
+    commitMessage: 'chore: release v${version}',
+    tagName: 'v${version}',
+  },
+  github: {
+    release: true,
+  },
+  npm: {
+    get publish() {
+      cachedHasPublishWorkflow ??= hasPublishWorkflow()
+      return !cachedHasPublishWorkflow
+    },
+  },
+}
+
+module.export = config
---
+++ packages/release-it-config/readme.md
@@ -0,0 +1,16 @@
+# @fisker/release-it-config
+
+> fisker's shareable release-it Configuration
+
+## Install
+
+```sh
+yarn add @fisker/release-it-config --dev
+```
+
+## Usage
+
+```js
+// .release-it.cjs
+module.exports = require('@fisker/release-it-config')
+```
