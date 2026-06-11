3e9a89408ff5e8cb4a992ffac190bf3a603487e4
fisker <lionkay@gmail.com>
2026-06-12 05:43:46 +0800

    Disable rules

--- packages/eslint-config/plugins/unicorn.js
+++ packages/eslint-config/plugins/unicorn.js
@@ -73,7 +73,7 @@ export default [
 
       'unicorn/relative-url-style': ['error', 'always'],
 
-      'unicorn/prefer-json-parse-buffer': 'error',
+      'unicorn/consistent-json-file-read': ['error', 'buffer'],
 
       'unicorn/switch-case-braces': ['error', 'avoid'],
 
@@ -94,6 +94,12 @@ export default [
           },
         },
       ],
+
+      'unicorn/better-dom-traversing': 'off',
+      'unicorn/consistent-compound-words': 'off',
+      'unicorn/prefer-https': 'off',
+      'unicorn/require-css-escape': 'off',
+      'unicorn/prefer-includes-over-repeated-comparisons': 'off',
     },
   },
 ]
